1a2bf6a586aeb9d8f84e9de8b7383dd200b55ad1
paulomd2 <paulo.sergio@agenciamd2.com.br>
2015-04-27 16:50:55 -0300

    postagem via twitter

--- postagem/index.php
+++ postagem/index.php
@@ -6,17 +6,18 @@ require_once './twitterPost.php';
 
 $posts = $objPostDao->listaPostAgora();
 
-foreach ($posts['twitter'] as $twitter) {
-    $imagem = '../upload/' . $twitter["imagem"];
-    twitterPost($twitter['textoTwitter'], $imagem);
-}
-
 foreach ($posts['instagram'] as $instagram) {
     $imagem = '../upload/' . $instagram["imagem"];
-    instagramPost($instagram['loginInstagram'], $instagram['senhaInstagram'], $instagram['textoInstagram'], $imagem);
+    instagramPost($instagram['loginInstagram'], $instagram['senhaInstagram'], $instagram['texto'], $imagem);
 }
+
+foreach ($posts['twitter'] as $twitter) {
+    $imagem = '../upload/' . $twitter["imagem"];
+    twitterPost($twitter['textoTwitter'], $imagem,$twitter['tokenTwitter'], $twitter['secretTwitter']);
+}
+
 
 foreach ($posts['facebook'] as $facebook) {
     $imagem = '../upload/' . $facebook["imagem"];
-    facebookPost($facebook['loginFacebook'], $facebook['senhaFacebook'], $facebook['textoFacebook'], $imagem);
+    facebookPost($facebook['loginFacebook'], $facebook['senhaFacebook'], $facebook['texto'], $imagem);
 }
