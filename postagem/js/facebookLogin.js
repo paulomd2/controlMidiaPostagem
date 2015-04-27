e975ddb1f0c694fcf2145d48551408781d353621
paulomd2 <paulo.sergio@agenciamd2.com.br>
2015-04-27 18:25:39 -0300

    atualização do login via facebook

--- postagem/js/facebookLogin.js
+++ postagem/js/facebookLogin.js
@@ -1,86 +1,10 @@
-/*
- $('a.post-to-page').click(function (event) {
- 
- });
- 
- function post_to_page(access_token_data) {
- var body = 'Reading JS SDK documentation';
- FB.api(
- '/page_id/feed',
- 'post', {
- message: body,
- access_token: access_token_data
- },
- function (response) {
- if (!response || response.error) {
- console.log(response.error);
- } else {
- console.log(response.id);
- }
- });
- }
- 
- 
- **************** 
- 
- function statusChangeCallback(response) {
- //    console.log('statusChangeCallback');
- //    console.log(response);
- if (response.status === 'connected') {
- testAPI();
- 
- //window.location = 'postFacebook.php';
- } else if (response.status === 'not_authorized') {
- document.getElementById('status').innerHTML = 'Please log ' +
- 'into this app.';
- } else {
- document.getElementById('status').innerHTML = 'Please log ' +
- 'into Facebook.';
- }
- }
- 
- function checkLoginState() {
- FB.getLoginStatus(function (response) {
- //if (response.status !== 'connected') {
- statusChangeCallback(response);
- //}
- });
- }
- 
- 
- 
- (function (d, s, id) {
- var js, fjs = d.getElementsByTagName(s)[0];
- if (d.getElementById(id))
- return;
- js = d.createElement(s);
- js.id = id;
- js.src = "//connect.facebook.net/en_US/sdk.js";
- fjs.parentNode.insertBefore(js, fjs);
- }(document, 'script', 'facebook-jssdk'));
- 
- function testAPI() {
- FB.api('/me', function (response) {
- document.getElementById('status').innerHTML =
- 'Thanks for logging in, ' + response.name + '!';
- });
- }
- 
- function logout() {
- FB.logout(function (response) {
- // user is now logged out
- });
- }
- checkLoginState();
- */
-
 window.fbAsyncInit = function () {
     FB.init({
         appId: '640961876010371',
         xfbml: true,
         version: 'v2.3',
-        cookie: true, 
-        xfbml: true, 
+        cookie: true,
+        xfbml: true,
     });
 
     checkLoginState()
@@ -111,29 +35,33 @@ function login() {
     }, {scope: 'public_profile,email,manage_pages,publish_actions'}
     );
 }
-/*
- function statusChangeCallback() {
- FB.getLoginStatus(function (response) {
- if (response.status === 'connected') {
- //console.log(response.authResponse.accessToken);
- //post_to_page(response.authResponse.accessToken);
- console.log(response.authResponse);
- 
- return response.authResponse.accessToken;
- }
- });
- }
- */
 function checkLoginState() {
+    userid = '';
+    accessToken = '';
+
     FB.getLoginStatus(function (response) {
         if (response.status !== 'connected') {
             login();
         } else {
-            console.log(response.authResponse.accessToken);
+            userid = response.authResponse.userID;
+            accessToken = response.authResponse.accessToken;
+
+            console.log('ID: ' + userid + ' token: ' + accessToken);
+
 
             return response.authResponse.accessToken;
         }
     });
+
+    FB.api("/{page-id}",
+    function (response) {
+        console.log(response);
+        
+        if (response && !response.error) {
+            /* handle the result */
+        }
+    });
+
 }
 
 function logout() {
